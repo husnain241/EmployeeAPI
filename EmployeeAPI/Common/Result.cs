720594d5e7f4df02a10023b98e38f7eaf031cc9f
Ali Husnain <alihusnaintcf970@gmail.com>
2026-02-10 11:03:57 +0500

    Create a Result class that response will be with information

---
+++ EmployeeAPI/Common/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeAPI.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        // Helper methods for easy use
+        public static Result<T> Success(T data, string message = "Success")
+            => new Result<T> { IsSuccess = true, Data = data, Message = message };
+
+        public static Result<T> Failure(string message)
+            => new Result<T> { IsSuccess = false, Message = message };
+    }
+}
